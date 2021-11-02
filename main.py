a99ff528fbe6ab14b296d522ca591685f2a63707
dkaiser11 <kaiserdavid9@gmail.com>
2021-11-03 00:05:05 +0100

    refactored imports

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import json
 import random
 
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.expected_conditions import \
-    presence_of_element_located
-from selenium.webdriver.support.ui import WebDriverWait
 
 BASE_URL = "https://www.kanjidamage.com/kanji/"
 
