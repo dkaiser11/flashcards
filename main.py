2b654c27357f94b3dd1ae4edd18bf356cff33854
dkaiser11 <kaiserdavid9@gmail.com>
2021-11-03 00:02:51 +0100

    site loading behaviour and multiple boxes learning

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import datetime
+import json
+import random
+
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.expected_conditions import \
+    presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-import random
-import json
-import datetime
 
 BASE_URL = "https://www.kanjidamage.com/kanji/"
 
@@ -32,36 +35,57 @@ def box(i: int):
     return data["boxes"][i]
 
 
+# moves card from box i to box j
+def move(card: int, i: int, j: int):
+    # normalize indeces
+    if i < 0:
+        i = 0
+    elif j < 0:
+        j = 0
+    elif i >= len(data["boxes"]):
+        i = len(data["boxes"]) - 1
+    elif j >= len(data["boxes"]):
+        j = len(data["boxes"]) - 1
+
+    # move card
+    box(i)["cards"].remove(card)
+    box(j)["cards"].append(card)
+
+    update()
+
+
 input_ = input()
 
 with WebDriver() as driver:
-    # load kanji list and remove hints
+    # load kanji list
     def load_main():
         driver.get(BASE_URL)
-        rows = driver.find_elements(By.TAG_NAME, 'tr')
-        txts = [row.find_elements(By.TAG_NAME, 'td')[3] for row in rows]
-        for txt in txts:
-            driver.execute_script("""
-            var txt = arguments[0];
-            txt.parentNode.removeChild(txt);
-            """, txt)
 
     # load kanji in box
-    def load(box):
-        load_main()
-        for card in random.sample(box["cards"], len(box["cards"])):
+    def load(i):
+        box_ = box(i)
+
+        for card in random.sample(box_["cards"], len(box_["cards"])):
+            load_main()
+            number = driver.find_element(
+                By.XPATH, f"//td[contains(text(), '{card}')]")
+            number.location_once_scrolled_into_view
             print(str(card))
             input()
             driver.get(BASE_URL + str(card))
 
+            # sort cards by if they were learned
             known = input()
             if known == "no" or known == "n":
-                raise NotImplementedError
+                move(card, i, i - 1)
             else:
-                raise NotImplementedError
+                move(card, i, i + 1)
 
     if input_ == "add":
         add()
-
+        for i in range(len(data["boxes"]) - 1, -1, -1):
+            day = int(datetime.datetime.now().strftime("%j"))
+            if day % box(i)["repetition_time"] == 0:
+                load(i)
     else:
-        load(box(0))
+        load(0)
