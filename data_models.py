d5740ea7d0b1248f7decd682cb83109d7097e222
dkaiser11 <kaiserdavid9@gmail.com>
2021-11-11 19:30:08 +0100

    complete rework w/ data model

--- data_models.py
+++ data_models.py
@@ -1,21 +1,37 @@
+import json
+
+
 class Data:
-    def __init__(self, json: dict = None) -> None:
-        if json == None:
+    def __init__(self, json_: str = None) -> None:
+        if json_ == None:
             self.learned = 0
             self.boxes = [Box(i) for i in [1, 3, 7, 30, 100, 1000]]
         else:
-            self.learned = json["learned"]
-            self.boxes = [Box(box) for box in json["boxes"]]
+            json_ = json.loads(json_)
+            self.learned = json_["learned"]
+            self.boxes = [Box(box["repetition_time"], box["cards"])
+                          for box in json_["boxes"]]
 
-    def to_json(self) -> dict:
-        return {"learned": self.learned, "boxes": [
-            {"repetition_time": box.repetition_time, "cards": box.cards} for box in self.boxes]}
+    def to_json(self) -> str:
+        return json.dumps({"learned": self.learned, "boxes": [
+            {"repetition_time": box.repetition_time, "cards": box.cards} for box in self.boxes]})
+
+    def add(self) -> None:
+        self.learned += 1
+        for i in range(10*(self.learned - 1) + 1, 10*self.learned + 1):
+            self.boxes[0].cards.append(i)
+
+    def move(self, card: int, i: int, j: int) -> None:
+        i = 0 if i < 0 else i
+        i = len(self.boxes) - 1 if i > len(self.boxes) - 1 else i
+        j = 0 if j < 0 else j
+        j = len(self.boxes) - 1 if j > len(self.boxes) - 1 else j
+
+        self.boxes[i].cards.remove(card)
+        self.boxes[j].cards.append(card)
 
 
 class Box:
-    def __init__(self, box: dict) -> None:
-        self.repetition_time = int(box["repetition_time"])
-        self.cards = list(box["cards"])
-
-    def __init__(self, repetition_time: int) -> None:
-        self.repetition_time = repetition_time
+    def __init__(self, repetition_time: int, cards: list = None) -> None:
+        self.repetition_time = int(repetition_time)
+        self.cards = [] if cards == None else cards
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import datetime
-import json
 import random
 import os
 
@@ -7,15 +5,19 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-from data_models import Data
+from data_models import *
+
+
+def cls() -> None:
+    os.system("cls")
+
 
 BASE_URL = "https://www.kanjidamage.com/kanji/"
 PATH = "cards.json"
 
-# load data
 with open(PATH, "r") as rdoc:
     try:
-        data = Data(json.loads(rdoc.read()))
+        data = Data(rdoc.read())
     except:
         with open(PATH, "w") as wdoc:
             wdoc.write(Data().to_json())
@@ -27,9 +29,12 @@ def update():
 
 
 def add():
-    data.learned += 1
-    [data.boxes[0].append(i) for i in range(
-        10*(data.learned - 1) + 1, 10*data.learned + 1)]
+    data.add()
+    update()
+
+
+def move(card: int, i: int, j: int) -> None:
+    data.move(card, i, j)
     update()
 
 
@@ -37,16 +42,61 @@ options = Options()
 options.add_argument("--log-level=3")
 
 with WebDriver(options=options) as driver:
+    def load(i: int = None, data_: Data = None) -> None:
+        i = 0 if i == None else i
+
+        data_ = Data(data.to_json()) if data_ == None else data_
+        box_ = data_.boxes[i]
+        cards_ = list(random.sample(box_.cards, len(box_.cards)))
+
+        for card in cards_:
+            learn(card, i)
+
+    def load_all() -> None:
+        data_ = Data(data.to_json())
+        for i in range(len(data_.boxes)):
+            if data_.learned % data_.boxes[i].repetition_time:
+                load(i, data_)
+
+    def learn(card: int, i: int) -> None:
+        cls()
+        print(card)
+
+        number = driver.find_element(
+            By.XPATH, f"//td[contains(text(), '{card if card == 1 else card - 1}')]")
+        number.location_once_scrolled_into_view
+
+        input()
+        driver.get(BASE_URL + str(card))
+
+        number = driver.find_element(
+            By.XPATH, f"//a[contains(text(), 'Next')]")
+        number.location_once_scrolled_into_view
+
+        learned = input()
+
+        options = {
+            "": 1,
+            "j": 1,
+            "y": 1,
+            "n": -1
+        }
+
+        move(card, i, i + options[learned])
+
+        driver.get(BASE_URL)
 
     driver.get(BASE_URL)
 
-    number = driver.find_element(
-        By.XPATH, f"//td[contains(text(), '{card if card == 1 else card - 1}')]")
-    number.location_once_scrolled_into_view
+    cls()
 
-    os.system("cls")
-    driver.get(BASE_URL + str(card))
+    mode = input()
 
-    number = driver.find_element(
-        By.XPATH, f"//a[contains(text(), 'Next')]")
-    number.location_once_scrolled_into_view
+    modes = {
+        "a": [add, quit],
+        "al": [add, load],
+        "l": [load],
+        "": [add, load_all]
+    }
+
+    [func() for func in modes[mode]]
